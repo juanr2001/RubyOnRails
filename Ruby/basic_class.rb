114007d931d5013f7e8cf7d075d91f5fc622a202
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-08-27 16:39:21 -0400

    This is just a basic class

---
+++ Ruby/basic_class.rb
@@ -0,0 +1,16 @@
+class Name
+    def title
+        "Mr."
+    end
+    def first_name
+        "Juan"
+    end
+    def last_name
+        "Ordaz"
+    end
+end
+
+name = Name.new
+puts name.title
+puts name.first_name
+puts name.last_name
